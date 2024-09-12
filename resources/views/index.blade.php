898bd8b5d068ffc0f457915d4a7a118cc8da17be
egu-snow <moemoemoerin@outlook.com>
2024-09-13 00:32:13 +0900

    共通テンプレート追加、bootstrap追加

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>アカウント一覧</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
+
     <h1>アカウント一覧</h1>
 
     @foreach ($acounts as $acount)
@@ -16,5 +11,5 @@
             </a>
         </p>
     @endforeach
-</body>
-</html>
+
+@endsection
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>詳細画面</title>
-</head>
-<body>
-    
+@extends('layout')
+
+@section('content')  
     <h2>{{ $site_name }}</h2>
 
     <p>ログインID：{{ $login_id }}</p>
@@ -19,5 +12,4 @@
         <a href={{ route('acount.list') }}>一覧に戻る</a>
     </p>
 
-</body>
-</html>
+@endsection
