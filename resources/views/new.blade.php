73ab1fbf4654e7aab251dcb0eb886fef61b63aa5
egu-snow <moemoemoerin@outlook.com>
2024-09-16 02:13:09 +0900

    入力必須の表示を作成

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -21,7 +21,7 @@
         @csrf
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">site name</span>
-            <input type="text" name="site_name" class="form-control" aria-describedby="basic-addon1">
+            <input type="text" name="site_name" value="--必須--" class="form-control" aria-describedby="basic-addon1">
         </div>
         <div class="input-group mb-3">
             <span class="input-group-text" id="basic-addon1">login ID</span>
@@ -41,7 +41,7 @@
         </div>
     
         <div class="mb-3">
-            <label for="category_numb">Category</label>
+            <label for="category_numb">Category<span class="text-danger ms-1">--必須--</span></label>
             <select name="category_numb" class="form-select">
                 <option selected>-- 選択してください --</option>
                 <option value="1">金融サービス</option>
@@ -52,9 +52,13 @@
             </select>
         </div>
     
-        <div class="mb-5">
+        
+        <div class="d-flex mb-5">
             <button type="submit" class="btn btn-outline-primary me-3">作成する</button>
-            <a href="{{ route('acount.list') }}">一覧に戻る</a>
+            <div class="float-start ms-3 mt-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M9.53 2.22a.75.75 0 0 0-1.06 0L2.22 8.47a.75.75 0 0 0 0 1.06l6.25 6.25a.75.75 0 0 0 1.06-1.06L4.56 9.75H16c.797 0 2.008.245 3 .959.952.686 1.75 1.835 1.75 3.791s-.798 3.105-1.75 3.791c-.992.714-2.203.959-3 .959h-3a.75.75 0 0 0 0 1.5h3c1.037 0 2.575-.305 3.876-1.241 1.339-.964 2.374-2.565 2.374-5.009 0-2.444-1.035-4.045-2.374-5.009C18.575 8.555 17.036 8.25 16 8.25H4.56l4.97-4.97a.75.75 0 0 0 0-1.06Z"></path></svg>
+                <a href="{{ route('acount.list') }}" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover">一覧に戻る</a>
+            </div>
         </div>
     </form>
 
