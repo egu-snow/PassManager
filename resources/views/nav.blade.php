6df35b8c6d6ebaa3ecfc00d3899e5dfd0fc965d0
egu-snow <moemoemoerin@outlook.com>
2024-09-16 16:49:29 +0900

    welcomeページのデフォルトを削除、共通レイアウトに認証機能を組み込み

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,3 +1,33 @@
-<nav class="navbar navbar-expand-md fixed-top" style="background-color: #44769a;">
+<nav class="px-3 navbar fixed-top" style="background-color: #44769a;">
     <a class="navbar-brand ps-2 text-light" href={{ route('acount.list') }}>pass manager</a>
+    <div>
+        @if (Route::has('login'))
+                            <div class="flex justify-end">
+                                @auth
+                                    <a
+                                        href="{{ url('/dashboard') }}"
+                                        class="link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                                    >
+                                        Dashboard
+                                    </a>
+                                @else
+                                    <a
+                                        href="{{ route('login') }}"
+                                        class="px-2 link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                                    >
+                                        Log in
+                                    </a>
+
+                                    @if (Route::has('register'))
+                                        <a
+                                            href="{{ route('register') }}"
+                                            class="px-2 link-light link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                                        >
+                                            Register
+                                        </a>
+                                    @endif
+                                @endauth
+                                    </div>
+                        @endif
+    </div>
 </nav>
