2ecc204e8b43a75ecbcac16fdbc4add6d523e785
egu-snow <moemoemoerin@outlook.com>
2024-09-24 20:17:55 +0900

    秘密の質問入力フォームを追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,19 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <p class="mt-4">『秘密の質問』はアカウント情報を表示する際に必要です。</p>
+        <!--秘密の質問-->
+        <div class="mt-4">
+            <x-input-label for="security_question" :value="__('秘密の質問')" />
+            <x-text-input id="security_question" class="block mt-1 w-full" type="text" name="security_question" :value="old('security_question')" required/>
+            <x-input-error :messages="$errors->get('security_question')" class="mt-2" />
+        </div>
+        <!--秘密の質問の答え-->
+        <div class="mt-4">
+            <x-input-label for="security_answer" :value="__('秘密の質問の答え')" />
+            <x-text-input id="security_answer" class="block mt-1 w-full" type="text" name="security_answer" :value="old('security_answer')" required/>
+            <x-input-error :messages="$errors->get('security_answer')" class="mt-2" />
+        </div>
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
